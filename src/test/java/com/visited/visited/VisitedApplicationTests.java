7e100c5abeb55e2982d82a6a3bcd47cad7f2508b
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-01-05 14:34:12 +0900

    init

---
+++ src/test/java/com/visited/visited/VisitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visited.visited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
