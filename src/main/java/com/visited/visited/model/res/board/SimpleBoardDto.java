ad3957f87488b3ced8d204a6fec6d4a80c5ab300
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-01-06 14:30:41 +0900

    [add] boardDto

---
+++ src/main/java/com/visited/visited/model/res/board/SimpleBoardDto.java
@@ -0,0 +1,12 @@
+package com.visited.visited.model.res.board;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SimpleBoardDto {
+	private Long id;
+	private String title;
+	private Long view;
+}
