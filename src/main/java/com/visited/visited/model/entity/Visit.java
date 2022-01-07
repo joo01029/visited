247ba9271eb7a7b2efdd518729676231c49f56ed
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-01-07 16:47:25 +0900

    [fix] entity attribute name

--- src/main/java/com/visited/visited/model/entity/Visit.java
+++ src/main/java/com/visited/visited/model/entity/Visit.java
@@ -1,21 +1,23 @@
 package com.visited.visited.model.entity;
 
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
 
 @Getter
-@RedisHash(value = "visit", timeToLive = 10*60)
+@ToString
+@RedisHash(value = "visit", timeToLive = 5*60)
 public class Visit {
 	@Id
 	private String id;
-	private Long BoardId;
+	private Long boardId;
 	private LocalDateTime createAt;
 
 	public Visit(Long boardId){
-		this.BoardId = boardId;
+		this.boardId = boardId;
 		this.createAt = LocalDateTime.now();
 	}
 }
