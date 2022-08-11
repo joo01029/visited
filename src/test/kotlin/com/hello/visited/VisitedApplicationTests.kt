4a2ba0223290bbaa0da1956b8c695bc045b62d5c
juyeong.nam <juyeong.nam@toss.im>
2022-08-11 21:30:46 +0900

    init

---
+++ src/test/kotlin/com/hello/visited/VisitedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hello.visited
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VisitedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
