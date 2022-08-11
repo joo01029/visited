94a43bd8e3fc132594485b5b179759ed2629c51f
juyeong.nam <juyeong.nam@toss.im>
2022-08-11 21:48:05 +0900

    split module

---
+++ batch/src/main/kotlin/com/hello/visited/VisitedApplication.kt
@@ -0,0 +1,11 @@
+package com.hello.visited
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VisitedApplication
+
+fun main(args: Array<String>) {
+    runApplication<VisitedApplication>(*args)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "visited"
+
+include(
+        "api",
+        "core",
+        "batch"
+)
