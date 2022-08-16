442ace327b67ccf0440bd6e4c7dfbb082f961e5b
juyeong.nam <juyeong.nam@toss.im>
2022-08-16 23:21:18 +0900

    add baseentity

---
+++ core/src/main/kotlin/com/hello/visited/data/BaseEntity.kt
@@ -0,0 +1,19 @@
+package com.hello.visited.data
+
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "REGISTERDT", updatable = false)
+    val createdAt: LocalDateTime? = null
+
+    @UpdateTimestamp
+    @Column(name = "MODIFIEDDT")
+    val updatedAt: LocalDateTime? = null
+}
