f903da5bb27be54aba4eb8db7b10f2f8f8a974bb
juyeong.nam <juyeong.nam@toss.im>
2022-08-20 19:52:41 +0900

    [add] api setting

---
+++ api/build.gradle.kts
@@ -0,0 +1,16 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+
+dependencies{
+    implementation(project(":core"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+}
