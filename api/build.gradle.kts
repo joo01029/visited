21296ac370252f2d878c3429b3b224cb1275680e
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-08-21 23:56:58 +0900

    Revert "[update] userimplement"
    This reverts commit e17f4fe973ccb8de65acb2b1c1e1f3b843eca5aa.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,16 +1,16 @@
-val commonVersion: String by project
-
-dependencies {
-    implementation(project(":core"))
-
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-}
-
 tasks.getByName("bootJar") {
     enabled = true
 }
 
 tasks.getByName("jar") {
     enabled = false
+}
+
+
+dependencies{
+    implementation(project(":core"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
 }
--- api/src/main/kotlin/com/hello/visited/VisitedApplication.kt
+++ api/src/main/kotlin/com/hello/visited/VisitedApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class VisitedApplication
+
 fun main(args: Array<String>) {
     runApplication<VisitedApplication>(*args)
 }
