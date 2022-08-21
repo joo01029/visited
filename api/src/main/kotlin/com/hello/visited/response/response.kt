b8214364236004eb46641ecebd2b5882e3eb44f3
juyeong.nam <juyeong.nam@toss.im>
2022-08-21 15:13:10 +0900

    [add] default response

---
+++ api/src/main/kotlin/com/hello/visited/response/response.kt
@@ -0,0 +1,42 @@
+package com.hello.visited.response
+
+class DefaultResponse<S> {
+    val result: ResultType
+    val data: S?
+    val error: String?
+
+    constructor(data: S){
+        this.result = ResultType.SUCCESS
+        this.data  = data
+        this.error = null
+    }
+
+    constructor(result: ResultType, data : S? = null, error:String? = null) {
+        this.result = result
+        this.data = data
+        this.error = error
+    }
+
+    companion object{
+        fun<S> success(): DefaultResponse<S>{
+            return DefaultResponse(ResultType.SUCCESS)
+        }
+
+        fun<S> success(data:S): DefaultResponse<S>{
+            return DefaultResponse(data)
+        }
+
+        fun <S> error(): DefaultResponse<S> {
+            return DefaultResponse(ResultType.ERROR)
+        }
+
+        fun <S> error(error:String): DefaultResponse<S> {
+            return DefaultResponse(result = ResultType.ERROR, error=error)
+        }
+    }
+}
+
+enum class ResultType{
+    SUCCESS,
+    ERROR
+}
