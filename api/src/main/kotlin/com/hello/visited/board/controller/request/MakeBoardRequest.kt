42c57fe7f1565185ca3ff35e0e860751c77fa53b
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-09-17 22:01:44 +0900

    [update]userDto 위치 변경

--- api/src/main/kotlin/com/hello/visited/board/controller/request/MakeBoardRequest.kt
+++ api/src/main/kotlin/com/hello/visited/board/controller/request/MakeBoardRequest.kt
@@ -1,9 +1,8 @@
 package com.hello.visited.board.controller
 
 import com.hello.visited.board.domain.BoardDto
-import com.hello.visited.board.domain.UserDto
+import com.hello.visited.user.domain.UserDto
 import org.hibernate.validator.constraints.Length
-import org.jetbrains.annotations.NotNull
 
 class MakeBoardRequest {
     @Length(min = 2, max = 20, message = "제목은 2~20글자 사이로 만들어야 합니다.")
@@ -21,7 +20,7 @@ class MakeBoardRequest {
 
     }
 
-    fun toUserDto():UserDto{
+    fun toUserDto(): UserDto {
         return UserDto(
             name = this.userName!!
         )
