f207fcca60ff2e45c5a1591e5a42f136ad7bd2ad
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-10-03 17:05:54 +0900

    [add] get board service

--- api/src/main/kotlin/com/hello/visited/board/controller/BoardController.kt
+++ api/src/main/kotlin/com/hello/visited/board/controller/BoardController.kt
@@ -1,7 +1,9 @@
 package com.hello.visited.board.controller
 
+import com.hello.visited.board.controller.response.GetBoardResponse
 import com.hello.visited.board.domain.BoardService
 import com.hello.visited.response.DefaultResponse
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,5 +19,12 @@ class BoardController (
     fun makeBoard(@Valid request : MakeBoardRequest): DefaultResponse<Nothing>{
         boardService.makeBoard(request.toMakeBoardSource(), request.toUserSource())
         return DefaultResponse.success()
+    }
+
+    @GetMapping
+    fun getBoards(): DefaultResponse<GetBoardResponse> {
+        val result = boardService.getBoards();
+
+        return DefaultResponse.success(GetBoardResponse.create(result))
     }
 }
