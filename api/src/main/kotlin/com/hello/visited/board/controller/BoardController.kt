2cef68cb394cebe5d5c3df9c22c2a8c8bad76aae
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-10-03 17:15:11 +0900

    [fix] cant make board using request body

--- api/src/main/kotlin/com/hello/visited/board/controller/BoardController.kt
+++ api/src/main/kotlin/com/hello/visited/board/controller/BoardController.kt
@@ -5,6 +5,7 @@ import com.hello.visited.board.domain.BoardService
 import com.hello.visited.response.DefaultResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -16,7 +17,7 @@ class BoardController (
     ) {
 
     @PostMapping
-    fun makeBoard(@Valid request : MakeBoardRequest): DefaultResponse<Nothing>{
+    fun makeBoard(@Valid @RequestBody request : MakeBoardRequest): DefaultResponse<Nothing>{
         boardService.makeBoard(request.toMakeBoardSource(), request.toUserSource())
         return DefaultResponse.success()
     }
